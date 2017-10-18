54f787c16fdec7c4b705e84d80d95fdbc0d557a0
Takashi Kokubun <takashikkbn@gmail.com>
2017-10-18 22:01:26 +0900

    mjit_compile.c: hook JIT in JIT-ed method call
    .travis.yml: Allow test-all failure

--- mjit_compile.c
+++ mjit_compile.c
@@ -60,7 +60,7 @@ fprint_call_method(FILE *f, VALUE ci, VALUE cc, unsigned int result_pos)
 {
     fprintf(f, "    {\n");
     fprintf(f, "      VALUE v = (*((CALL_CACHE)0x%"PRIxVALUE")->call)(th, cfp, &calling, 0x%"PRIxVALUE", 0x%"PRIxVALUE");\n", cc, ci, cc);
-    fprintf(f, "      if (v == Qundef) {\n"); /* TODO: also call jit_exec */
+    fprintf(f, "      if (v == Qundef && (v = mjit_exec(th)) == Qundef) {\n"); /* TODO: also call jit_exec */
     fprintf(f, "        VM_ENV_FLAGS_SET(th->ec.cfp->ep, VM_FRAME_FLAG_FINISH);\n"); /* This is vm_call0_body's code after vm_call_iseq_setup */
     fprintf(f, "        stack[%d] = vm_exec(th);\n", result_pos);
     fprintf(f, "      } else {\n");
