fa8d45f6815b3d0be96ec94c7c13427e3aef0dc5
Takashi Kokubun <takashikkbn@gmail.com>
2017-10-29 20:37:18 +0900

    Revert "vm_insnhelper.c: resurrect large inline"
    This reverts commit 280927f82c61853fabca9f17920fcef9f1a96088.

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -356,6 +356,8 @@ vm_env_write(const VALUE *ep, int index, VALUE v)
     }
 }
 
+#ifndef MJIT_HEADER
+
 void
 rb_vm_env_write(const VALUE *ep, int index, VALUE v)
 {
@@ -382,8 +384,6 @@ rb_vm_bh_to_procval(rb_thread_t *th, VALUE block_handler)
 	}
     }
 }
-
-#ifndef MJIT_HEADER
 
 /* svar */
 
@@ -1321,10 +1321,11 @@ vm_expandarray(rb_control_frame_t *cfp, VALUE ary, rb_num_t num, int flag)
     RB_GC_GUARD(ary);
 }
 
+#ifndef MJIT_HEADER
+
 static VALUE vm_call_general(rb_thread_t *th, rb_control_frame_t *reg_cfp, struct rb_calling_info *calling, const struct rb_call_info *ci, struct rb_call_cache *cc);
 
-ALWAYS_INLINE(static void vm_search_method(const struct rb_call_info *, struct rb_call_cache *, VALUE));
-static inline void
+RUBY_FUNC_EXPORTED void
 vm_search_method(const struct rb_call_info *ci, struct rb_call_cache *cc, VALUE recv)
 {
     VALUE klass = CLASS_OF(recv);
@@ -1349,6 +1350,8 @@ vm_search_method(const struct rb_call_info *ci, struct rb_call_cache *cc, VALUE 
     cc->class_serial = RCLASS_SERIAL(klass);
 #endif
 }
+
+#endif /* #ifndef MJIT_HEADER */
 
 static inline int
 check_cfunc(const rb_callable_method_entry_t *me, VALUE (*func)())
