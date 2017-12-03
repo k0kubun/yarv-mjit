767cfbc9c3c72caa40557bb8662a62df75e6868c
Takashi Kokubun <takashikkbn@gmail.com>
2017-12-04 00:45:30 +0900

    Revert "generate_mjit_header.rb: manually import some macros"
    This reverts commit e3f2032c234ff5dff85b7373ef017f44db6d73ae.

--- tool/generate_mjit_header.rb
+++ tool/generate_mjit_header.rb
@@ -3,8 +3,6 @@
 #
 # This will output preprocessed <source>.c to <builddir>/<source>.i.
 
-require 'shellwords'
-
 builddir = ARGV.shift
 src = ARGV.shift
 if ARGV.size < 2 || !src.end_with?('.c')
@@ -14,29 +12,14 @@ end
 
 cc = ARGV.shift
 args = [src, *ARGV]
-dest = File.join(builddir, File.basename(src).sub(/\.c\z/, '.i'))
 
 case cc
 when /\Acl(|\.exe)\z/
   # cl.exe outputs result to *.i
-  args.push('-P', '-EP')
-
-  # workaround: This manually preserves macros only for cl.exe.
-  # cl.exe can't keep macro definitions with /P.
-  ruby_h = File.read(File.expand_path("../include/ruby/ruby.h", __dir__))
-  suffix = ruby_h.gsub(/^(?!#define ).+$/, '').gsub(/^.+\\$/, '')
+  args.push('-P')
 else # for gcc, clang
+  dest = File.join(builddir, File.basename(src).sub(/\.c\z/, '.i'))
   args.push('-o', dest, '-E', '-dD') # -dD is for clang
-  suffix = nil
 end
 
-cmd = [cc, *args].shelljoin
-unless system(cmd)
-  abort "Failed to execute: #{cmd}"
-end
-
-if suffix
-  File.open(dest, 'a') do |f|
-    f.puts suffix
-  end
-end
+Process.exec(cc, *args)
